5f11255559937946dd4789a5bf848192780f6e60
Boobalan/28 <boobal7722@gmail.com>
2022-01-22 14:40:42 +0530

    first commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 style={{color:"blue", padding:"0px"}}>Add Task</h1>
+        </header>
+    );
+};
+
+export default Header;
